13e7a58b710e88594e2d608f53c3361cd0a0f8e5
Nathan Essex <nathan@essex.id.au>
2016-09-12 00:50:01 +1000

    Outline direction of the API going forward

--- storytrove/views/api.py
+++ storytrove/views/api.py
@@ -37,6 +37,12 @@ def getTagString(tags):
 
     return " AND ".join(filtered_tags)
 
+'''
+TODO: Move to model, this functionality will be sort-of replaced by a
+set of functions, primarily prompts()
+
+This will still be used internally to build up new sets of prompts from trove.
+'''
 def search(request):
     tags = request.GET.get('tags').split(',')
     reactions = request.GET.get('reactions').split(',')
@@ -63,4 +69,78 @@ def search(request):
     return JsonResponse({
         'success': True,
         'response': res
+    })
+
+'''
+Get the current set of prompts
+By default this should return a set of featured prompts for each of a few tags
+Results can also be filtered by providing an array of tags or reactions
+'''
+def prompts(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Get a list of recently written stories
+This is intended to be used to get a list of stories for the reading section
+Optional filters:
+tag
+reaction
+'''
+def recently_written(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Get a list of the most recent stories written by a particular user
+Requires a username
+'''
+def recent_by_author(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Get details for a prompt including image links, metadata, stories, votes and reactions
+Requires a prompt id
+'''
+def prompt(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Get details required to display a story including the story text, author details,
+prompt details, and comment metadata.
+Requires a story id
+'''
+def story(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Get an array of comments for a given story id
+'''
+def comments(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Add a reaction (by id) to a given story id
+'''
+def add_story_reaction(request):
+    return JsonResponse({
+        'failure': True
+    })
+
+'''
+Add a reaction (by id) to a given comment id
+'''
+def add_comment_reaction(request):
+    return JsonResponse({
+        'failure': True
     })
