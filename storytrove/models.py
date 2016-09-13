965b5e7734b04d06f3bc4dc3829ef3d63f35af57
Nathan <nathan@essex.id.au>
2016-09-13 12:52:10 +1000

    Fix model

--- storytrove/models.py
+++ storytrove/models.py
@@ -3,33 +3,33 @@ from django.db import models
 class UserAccount(models.Model):
     username = models.CharField(max_length = 15)
     password = models.CharField(max_length = 15)
-	image = models.CharField()
-	email_address = models.CharField(max_length = 50)
+    image = models.CharField(max_length = 255)
+    email_address = models.CharField(max_length = 50)
     last_login = models.DateField()
-	create_date = models.DateField()
+    create_date = models.DateField()
 
 class TroveObject(models.Model):
-    trove_id = models.CharField() # this could actually be an int field
-    description = models.CharField()
-    
+    trove_id = models.CharField(max_length = 20) # this could actually be an int field
+    description = models.TextField()
+
 class Prompt(models.Model):
     trove_objects = models.ManyToManyField(TroveObject)  #this is the list of trove ids used as stimuli
-	tags = models.CharField()
+    tags = models.CharField(max_length = 50)
 
 class Response(models.Model):
     user = models.ForeignKey(UserAccount, on_delete = models.CASCADE)
     prompt = models.ForeignKey(Prompt, on_delete = models.CASCADE)
     title = models.CharField(max_length = 100)
     date = models.DateField()
-    text = models.CharField() #attempted no maximum length
-	is_private = models.BooleanField()
-	is_draft = models.BooleanField()
+    text = models.TextField() #attempted no maximum length
+    is_private = models.BooleanField()
+    is_draft = models.BooleanField()
 
 class Comment(models.Model):
     user = models.ForeignKey(UserAccount, on_delete = models.CASCADE)
     response = models.ForeignKey(Response, on_delete = models.CASCADE)
     date = models.DateField()
-    text = models.CharField() #attempted no maximum length
+    text = models.TextField() #attempted no maximum length
 
 class EmojiResponseOnResponse(models.Model):
     user = models.ForeignKey(UserAccount, on_delete = models.CASCADE)
@@ -42,10 +42,10 @@ class EmojiResponseOnResponse(models.Model):
         ('2', ':cry:'),
         ('3', ':laughing:'),
         ('4', ':scream:'),
-		('5', ':thinking:')
+        ('5', ':thinking:')
     )
     emoji = models.CharField(max_length = 1, choices = EMOJI_RESPONSES)
-	
+
 class EmojiResponseOnComment(models.Model):
     user = models.ForeignKey(UserAccount, on_delete = models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete = models.CASCADE)
@@ -57,6 +57,6 @@ class EmojiResponseOnComment(models.Model):
         ('2', ':cry:'),
         ('3', ':laughing:'),
         ('4', ':scream:'),
-		('5', ':thinking:')
+        ('5', ':thinking:')
     )
     emoji = models.CharField(max_length = 1, choices = EMOJI_RESPONSES)
--- storytrove/urls.py
+++ storytrove/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from . import views
+from . import models
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
