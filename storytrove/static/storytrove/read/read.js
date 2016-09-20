8d21ba3a7ded4d8daaf02fdba4a18e812d619548
Nathan Essex <nathan@essex.id.au>
2016-09-20 19:55:56 +1000

    Tidy up read story page mockup

--- storytrove/static/storytrove/read/read.js
+++ storytrove/static/storytrove/read/read.js
@@ -35,7 +35,7 @@ class StoryBlock extends React.Component {
                 <a href="/story/example"><h2>My Story</h2></a>
                 <p>Snippet. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru&hellip;</p>
                 <div className="story-block-footer">
-                    <div className="btn-group story-block-controls" role="group" aria-label="story controls">
+                    <div className="btn-group button-row-controls" role="group" aria-label="story controls">
                         <button type="button" className="btn btn-secondary"><i className="fa fa-book" /> 4</button>
                         <button type="button" className="btn btn-secondary" onClick={this.addVote}><i className="fa fa-arrow-up" /> {this.state.votes}</button>
                         {/* Downvotes intentionally not shown */}
