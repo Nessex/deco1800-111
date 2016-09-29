8cb3170a6820dd6e1c694fba2ada13dc7e4e2a0c
Nathan Essex <nathan@essex.id.au>
2016-09-29 22:44:42 +1000

    Add user profile overview to main account page

--- storytrove/static/storytrove/account/account.js
+++ storytrove/static/storytrove/account/account.js
@@ -1,3 +1,17 @@
+class EmojiText extends React.Component {
+    render() {
+        const innerHTML = {
+            __html: emojione.shortnameToImage(this.props.value)
+        };
+
+        return (<span onClick={this.props.onClick} className={this.props.className}><span dangerouslySetInnerHTML={innerHTML} /></span>);
+    }
+}
+
+EmojiText.PropTypes = {
+    value: React.PropTypes.string
+};
+
 class Account extends React.Component {
     constructor(props) {
         super(props);
@@ -13,6 +27,20 @@ class Account extends React.Component {
                 <div className="row">
                     <div className="col-xs-12">
                         <h2>Manage Account</h2>
+                        <div className="row">
+                            <section className="col-xs-12 account-overview">
+                                <div className="pull-xs-left">
+                                    <img src="https://i.redd.it/yy2iqou5xjkx.jpg" className="edit-account-profile-picture" />
+                                </div>
+                                <div>
+                                    <h3>Harambe</h3>
+                                    Total Votes: 234<br />
+                                    <span><EmojiText value=":thumbsup:" /> 9 </span>
+                                    <span><EmojiText value=":joy:" /> 8 </span>
+                                    <span><EmojiText value=":cry:" /> 7 </span>
+                                </div>
+                            </section>
+                        </div>
                         <ul>
                             <li><a href="/account/edit/">Edit Details</a></li>
                             <li><a href="/account/achievements/">Achievements</a></li>
