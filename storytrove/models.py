d559e3d3ad19925a2aa6a424e520b90384a58c14
Jonathan Holland <jonathan.holland8@gmail.com>
2016-10-11 20:25:20 +1000

    Missing import for settings module to incorporate default User db

--- storytrove/models.py
+++ storytrove/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class TroveObjectManager(models.Manager):
