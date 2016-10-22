9f8106553327e64aae6fa457df92c0708f7b0ccc
Nathan Essex <nathan@essex.id.au>
2016-10-22 16:56:41 +1000

    Remove unused sample data from account stories

--- storytrove/static/storytrove/account/account_stories.js
+++ storytrove/static/storytrove/account/account_stories.js
@@ -37,21 +37,8 @@ class AccountStories extends React.Component {
         
         //Example data
         this.setState({
-            storyIds: ["1234", "5678"],
-            stories: {
-                "1234": {
-                    id: "1234",
-                    title: "My New Story",
-                    preview: "Lorem ipsum dolor sit amet. This is the first few sentences of the story.",
-                    url: "/story/1234"
-                },
-                "5678": {
-                    id: "5678",
-                    title: "My Second New Story",
-                    preview: "Lorem ipsum Dolore dolore dolor consectetur labore qui labore laboris veniam magna labore reprehenderit.",
-                    url: "/story/5678"
-                }
-            }
+            storyIds: [],
+            stories: {}
         });
     }
 
