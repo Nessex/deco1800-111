f7a4f8d60b02a968c7608d3f9550fe7625f4bee1
Nathan Essex <nathan@essex.id.au>
2016-10-22 14:21:33 +1000

    Update trove objects to ones with working work id's

--- storytrove/populateDB.py
+++ storytrove/populateDB.py
@@ -17,8 +17,16 @@ USER_ONE.save()
 USER_TWO.save()
 
 # Create Trove Objects
-TroveObject1 = TroveObject.objects.create_trove_object('137268923', "River Red Gums (Eucalyptus Camaldulensis)")
-TroveObject2 = TroveObject.objects.create_trove_object('234844423', "The Raymond Dinner Menu, March 11, 1917, Pasadena, California")
+TroveObject1 = TroveObject.objects.create_trove_object('209660950', "Felton Bequest, 1946")
+TroveObject2 = TroveObject.objects.create_trove_object('7466787', "1933. Two pantomine players, Mr J. Stocks and Mr harry Freeman (King and Queen) at the Urban District Council tenants annual gala.")
+TroveObject3 = TroveObject.objects.create_trove_object('13294658', "Glen Gala House")
+TroveObject4 = TroveObject.objects.create_trove_object('212481846', "Gala Cinema owner, Harry Waghorn in the projection room, 4 January 1988.")
+TroveObject5 = TroveObject.objects.create_trove_object('192301041', "Stanley, Tasmania, Australia")
+TroveObject6 = TroveObject.objects.create_trove_object('192305936', "Hobart, Tasmania, Australia")
+TroveObject7 = TroveObject.objects.create_trove_object('207535485', "Child's brown school case made of composite fibre by Duro Travel Goods, circa 1953-1965. The case has two names written on it: Jennifer Andison, crossed out and replaced with Bruce Andison. The Andison family lived in Lane Cove, Sydney until 1964. This school case was handed down to Bruce from his sister Jennifer. In 1964 the family moved to Melbourne where Bruce attended Veterinary School graduating in 1973. He then entered Medical School graduating in 1980, trained as an Obstetrician and Gynecologist and moved to the USA. Duro Travel Goods Ltd was registered in 1953 (according to the Companies Index held in the Queensland Archives); its name was delisted in 1983. Child's brown school case made of composite fibre, reinforced with metal edges and fibre corners and a wooden frame inside the lid. The case has a plastic carry handle and locks on either side. On the outside of the case, underneath the handle, are the initials \"B.A.\" in large lettering in silver paint. Inside the lid of the case is written \"IB 3542/​BRUCE ANDISON (\"Jennifer Andison\" crossed out) /​591/​Mowbray Rd/​Lane Cove\" Also inside is a small paper label which reads \"VULCANISED FIBRE/​MADE BY/​DURO/​TRAVEL GOODS LTD/​SYDNEY N.S.W.\"")
+TroveObject8 = TroveObject.objects.create_trove_object('16124264', "Office staff at World Travel Headquarters working on a Swingaway holiday promotion")
+TroveObject9 = TroveObject.objects.create_trove_object('210527547', "0G2A3940-2 Travellers travel photobook")
+
 
 TroveObject1.save()
 TroveObject2.save()
@@ -27,10 +35,20 @@ TroveObject2.save()
 Prompt1 = Prompt.objects.create_prompt("war")
 Prompt1.save()
 Prompt1.trove_objects.add(TroveObject1)
+Prompt1.trove_objects.add(TroveObject4)
+Prompt1.trove_objects.add(TroveObject7)
 
 Prompt2 = Prompt.objects.create_prompt("death")
 Prompt2.save()
 Prompt2.trove_objects.add(TroveObject2)
+Prompt2.trove_objects.add(TroveObject5)
+Prompt2.trove_objects.add(TroveObject8)
+
+Prompt2 = Prompt.objects.create_prompt("death")
+Prompt2.save()
+Prompt2.trove_objects.add(TroveObject3)
+Prompt2.trove_objects.add(TroveObject6)
+Prompt2.trove_objects.add(TroveObject9)
 
 # Set up Response texts
 ResponseTitle1 = "Old Dave"
