16277bf08c72cfd3d01941092f71ddcb85673f03
Nathan Essex <nathan@essex.id.au>
2016-10-26 12:06:16 +1000

    Space out homepage, make icons part of button.

--- storytrove/static/storytrove/home/home.js
+++ storytrove/static/storytrove/home/home.js
@@ -12,12 +12,16 @@ class Homepage extends React.Component {
             <div className="container homepage">
                 <div className="row m-t-3 m-b-3">
                     <div className="col-xs-6 text-xs-center">
-                        <p><i className="fa fa-pencil-square-o fa-5x" aria-hidden="true" /></p>
-                        <a href="/browse/" className="btn btn-primary">I want to write!</a>
+                        <a href="/browse/" className="btn btn-primary homepage-button">
+                            <p><i className="fa fa-pencil-square-o fa-5x" aria-hidden="true" /></p>
+                            <p>I want to write!</p>
+                        </a>
                     </div>
                     <div className="col-xs-6 text-xs-center">
-                        <p><i className="fa fa-book fa-5x" aria-hidden="true" /></p>
-                        <a href="/read/" className="btn btn-primary">I want to read!</a>
+                        <a href="/read/" className="btn btn-primary homepage-button">
+                            <p><i className="fa fa-book fa-5x" aria-hidden="true" /></p>
+                            <p>I want to read!</p>
+                        </a>
                     </div>
                 </div>
             </div>
